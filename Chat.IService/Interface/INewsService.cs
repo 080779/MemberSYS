c13cc65d273c4541fa09b3313bfd7e9a496ba13c
080779 <exceed295@qq.com>
2018-03-12 14:57:37 +0800

    2018-3-12v1

---
+++ Chat.IService/Interface/INewsService.cs
@@ -0,0 +1,33 @@
+﻿using Chat.DTO.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.IService.Interface
+{
+    public interface INewsService : IServiceSupport
+    {
+        //增加公告
+        long Add(string Title, int dropNewType, string Context, DateTime Time);
+
+        /// <summary>
+        /// 分页
+        /// </summary>
+        /// <param name="PageIndex"></param>
+        /// <param name="PageSize"></param>
+        /// <returns></returns>
+        NewsPageResult GetPageList(int PageIndex, int PageSize);
+        ///获取id
+        NewsDTO GetModel(long Id);
+        //更新
+        bool Update(NewsDTO hh);
+
+    }
+    public class NewsPageResult
+    {
+        public long TotalCount { get; set; }
+        public NewsDTO[] News { get; set; }
+    }
+}
